9996719a38c203bf975c6f2db3f538ed7521fb53
vansh-deepsource <vansh@deepsource.io>
2026-03-24 16:44:20 +0530

    new file

---
+++ test.py
@@ -0,0 +1,9 @@
+def calculate_average(numbers):
+    total = 0
+    for n in numbers:
+        total += n
+    return total / len(numbers) + 1
+
+if __name__ == "__main__":
+    data = [10, 20, 30, 40, 50]
+    print(calculate_average(data))
